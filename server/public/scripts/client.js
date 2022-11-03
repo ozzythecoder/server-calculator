3d1a68431d617fa2ad7c3ae8ff4d9c15d89dc544
August McAllister <august.chm@gmail.com>
2022-11-03 18:39:49 -0500

    Stored equations are appending to the DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,19 +2,29 @@ $( document ).ready(onReady)
 
 function onReady() {
   console.log('jQ');
-  render();
+  getStuff();
   $('#submit').on('click', doMath)
 }
 
-function render() {
+function getStuff() {
   $.ajax({
     method: 'GET',
     url: '/print'
   }).then( (res) => {
-    console.log('successful render');
+    console.log('successful render', res);
+    render(res);
   }).catch( (err) => {
     console.log('butts :c');
   })
+}
+
+function render(arr) {
+  for (let equation of arr) {
+    console.log(equation);
+    $( '#results-history' ).append(`
+      ${equation.num1} ${equation.oper} ${equation.num2} = ${equation.result}<br>
+  `)  
+  }
 }
 
 function doMath() {
