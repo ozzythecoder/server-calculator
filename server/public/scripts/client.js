06f2d7947fbf04cbccf86b7b7e7cfa8a59ee4841
August McAllister <august.chm@gmail.com>
2022-11-03 19:52:32 -0500

    Functional calculator achieved!

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,7 +3,19 @@ $( document ).ready(onReady)
 function onReady() {
   console.log('jQ');
   getStuff();
-  $('#submit').on('click', doMath)
+  $('#submit').on('click', sendEquation)
+}
+
+function render(arr) {
+
+  clearResults();
+
+  for (let equation of arr) {
+    $( '#results-history' ).prepend(`
+      ${equation.num1} ${equation.oper} ${equation.num2} = ${equation.result}<br>
+  `)
+  }
+
 }
 
 function getStuff() {
@@ -11,22 +23,35 @@ function getStuff() {
     method: 'GET',
     url: '/print'
   }).then( (res) => {
-    console.log('successful render', res);
     render(res);
   }).catch( (err) => {
     console.log('butts :c');
   })
 }
 
-function render(arr) {
-  for (let equation of arr) {
-    console.log(equation);
-    $( '#results-history' ).append(`
-      ${equation.num1} ${equation.oper} ${equation.num2} = ${equation.result}<br>
-  `)  
-  }
+function sendEquation() {
+
+  $.ajax({
+    method: 'POST',
+    url: '/math',
+    data: {
+      num1: $('#num1').val(),
+      num2: $('#num2').val(),
+      oper: $('#operator').val()
+    }
+  }).then((res) => {
+    console.log('post request successful!');
+    clearInputs();
+    getStuff();
+  }).catch((err) => {
+    console.log('bro whatttt');
+  })
+
 }
 
-function doMath() {
-  console.log('in doMath');
+function clearInputs() {
+  $( 'input' ).val();
+  $( 'select' ).val();
 }
+
+function clearResults() { $( '#results-history' ).empty() };
--- server/server.js
+++ server/server.js
@@ -21,9 +21,25 @@ let resultHistory = [
     result: 7 }
 ]
 
+const doTheMath = {
+  '+': function(a, b) { return a + b },
+  '-': function(a, b) { return a - b },
+  '*': function(a, b) { return a * b },
+  '/': function(a, b) { return a / b }
+}
+
 app.get('/print', (req, res) => {
   console.log('in /print route');
   res.send(resultHistory);
+})
+
+app.post('/math', (req, res) => {
+  console.log('in /math route');
+  let mathObj = req.body;
+  
+  mathObj.result = doTheMath[mathObj.oper](Number(mathObj.num1),Number(mathObj.num2))
+  resultHistory.push(mathObj);
+  res.sendStatus(200);
 })
 
 // set up listener
