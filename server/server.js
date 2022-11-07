04c8bc1d94f058948ca482665d2bd6ed569cc858
August McAllister <august.chm@gmail.com>
2022-11-07 11:55:46 -0600

    Added env variable for Heroku deployment

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 // set up express
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = process.env.port || 5000;
 app.use(express.static('server/public/'))
 
 // set up body parser
